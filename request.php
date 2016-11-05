676dc5e6b3286e60b49720a2068e6134f359a4b8
zaavanr <zaavanrichards@hotmail.com>
2016-11-04 22:27:40 -0500

    Request.php

---
+++ request.php
@@ -0,0 +1,17 @@
+
+<?php
+// accept a term (keyword)
+// respond with a value
+$query = $_GET['q'];
+$definition = [
+    "definition" => "A statement of the exact meaning of a word, especially in a dictionary.",
+    "bar" => "A place that sells alcholic beverages",
+    "ajax" => "Technique which involves the use of javascript and xml (or JSON)",
+    "html" => "The standard markup language for creating web pages and web applications.",
+    "css" => "A style sheet language used for describing the presentation of a document written in a markup language.",
+    "javascript" => "A lightweight, interpreted programming language with first-class functions that adds interactivity to your website.",
+    "php" => "A server-side scripting language, and a powerful tool for making dynamic and interactive websites",
+];
+print "<h3>" . strtoupper($query) . "</h3>";
+print "<p>" . $definition[$query] . "</p>";
+?>
